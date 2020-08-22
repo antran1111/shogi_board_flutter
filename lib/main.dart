e43a76830abda908d591bce429157f610bcea9c3
Kazufumi Suzuki <skazufumi.a02525@gmail.com>
2020-08-22 20:36:15 +0900

    firebaseの初期化不具合対応

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:shogi_board/screens/board_screen.dart';
 import 'package:shogi_board/screens/menu_screen.dart';
 import 'package:shogi_board/models/board_data.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //Firebase.initializeApp();
     return ChangeNotifierProvider<BoardData>(
       create: (_) => BoardData(),
       child: MaterialApp(
